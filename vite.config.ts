03aa9591614c84aca6b5d782c678b15e0524132a
Valentin Chantelot <contact@dev.chantelot.com>
2026-05-20 10:32:44 +0200

    chore(test): install Vitest et testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { resolve } from "node:path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./vitest.setup.ts"],
   },
 });
